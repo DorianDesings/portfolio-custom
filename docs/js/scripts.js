093213945f23a67c94e1ce060e9f2d8aac3fc123
Dorian <dorianoriginaldesings@gmail.com>
2021-09-07 00:08:35 +0200

    Multi-idioma listo

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -3,6 +3,9 @@ const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 const toggleColor = document.getElementById('toggle-color');
 
+const buttons = document.getElementById('buttons');
+const textsToChange = document.querySelectorAll('[data-section]');
+
 const rootStyles = document.documentElement.style;
 
 toggleTheme.addEventListener('click', () => {
@@ -19,5 +22,23 @@ toggleTheme.addEventListener('click', () => {
 toggleColor.addEventListener('click', e => {
   if (e.target.classList.contains('colors__item')) {
     rootStyles.setProperty('--primary-color', e.target.dataset.color);
+  }
+});
+
+const changeLanguage = async language => {
+  const requestJson = await fetch(`./languages/${language}.json`);
+  const texts = await requestJson.json();
+
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+    textToChange.textContent = texts[section][value];
+  }
+};
+
+buttons.addEventListener('click', e => {
+  const language = e.target.dataset.language;
+  if (language) {
+    changeLanguage(language);
   }
 });
